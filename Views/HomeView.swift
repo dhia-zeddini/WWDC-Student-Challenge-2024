bd9b29197dc5ac54c99e3bc2870dba8a84a083d0
dhia-zeddini <dhia.zeddini.14@gmail.com>
2024-02-08 14:18:58 +0100

    Initial Commit

---
+++ Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Zeddin Dhia on 8/2/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
